856550a816fad4989b946184dd882186303b3267
Fagbowore Adebola <bfagbowore@gmail.com>
2021-06-08 21:21:55 +0100

    coach registration

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>Registration</div>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form @save-data='saveData'></coach-form>
+    </base-card>
+  </section>
 </template>
 
 <script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
 export default {
-  name: 'CoachContact'
+  name: 'CoachContact',
+  components: {
+    CoachForm
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.replace('/coaches');
+    }
+  }
 };
 </script>
 
